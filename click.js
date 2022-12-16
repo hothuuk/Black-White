8b5806538bfad64d4909a8ed92af909b0064066d
hothuuk <s22014@gsm.hs.kr>
2022-12-16 22:14:53 +0900

    commit

---
+++ click.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+const startButton = document.getElementById('start-box');
+const fontBox = document.getElementById('font-box');
+const mader = document.getElementById('text1');
+
+startButton.onclick = function(event) {
+    startButton.style.backgroundColor = 'black';
+    startButton.style.visibility = 'hidden';
+
+    fontBox.style.transform = 'translate(-50%, 40%)';
+    mader.style.color = 'white';
+};
