da1d8cc84fe962286e7ee47c0b0db42ac270183e
hothuuk <s22014@gsm.hs.kr>
2022-12-17 18:40:54 +0900

    commit

--- imgChange.js
+++ imgChange.js
@@ -6,3 +6,11 @@ images[2] = '/images/003.png';
 
 var index = 0;
 
+const change = setInterval(function() {
+    if (index < images.length - 1) {
+        index++
+    } else {
+        index = 0;
+    }
+    img.src = images[index];
+}, 8000);
